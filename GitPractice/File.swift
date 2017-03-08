a8dd5dc19dc2d813ae4fac841332ac1d299eb6db
Parker Erikson <parkererikson@maed-2.local>
2017-03-08 10:40:02 -0700

    I DID THE THING

---
+++ GitPractice/File.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  GitPractice
+//
+//  Created by Parker Erikson on 3/8/17.
+//  Copyright © 2017 Allen welch. All rights reserved.
+//
+
+import Foundation
+
+class Woot {
+    var stuff : String
+    var things : String
+    
+    init(stuff: String, things:String) {
+        self.stuff = stuff
+        self.things = things
+    }
+}
