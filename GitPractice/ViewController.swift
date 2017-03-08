63344e8ac843376c0d52918c848e09b01e83a13c
allen welch <allenwelch@maed-5.local>
2017-03-08 09:55:09 -0700

    Initial Commit

---
+++ GitPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by allen welch on 3/8/17.
+//  Copyright © 2017 Allen welch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
